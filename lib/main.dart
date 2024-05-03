620891e25e8774cde2071a98acd09db015aa469c
Sreenu-Chandaka <chandakasreenu0@gmail.com>
2024-05-03 14:08:27 +0530

    Create| Email and Password fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:dostify/pages/login_page.dart';
 import 'package:dostify/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 import "package:firebase_analytics/firebase_analytics.dart";
 import 'services/navigation_service.dart';
 
 void main() {
-  runApp( SplashScreen(onIntializationComplete: () { MainApp(); }, key: UniqueKey(),));
+  runApp(
+    SplashScreen(
+      key: UniqueKey(),
+      onInitializationComplete: () {
+        runApp(
+          MainApp(),
+        );
+      },
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget{
@@ -12,9 +22,15 @@ class MainApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(title: 'Dostify',theme:ThemeData(backgroundColor: const Color.fromRGBO(36, 35, 49, 1),scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1),bottomNavigationBarTheme: const BottomNavigationBarThemeData(backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),)
+   return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Dostify',theme:ThemeData(backgroundColor: const Color.fromRGBO(36, 35, 49, 1),scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1),bottomNavigationBarTheme: const BottomNavigationBarThemeData(backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),)
    ),
-   navigatorKey:NavigationService.navigatorKey
+   navigatorKey:NavigationService.navigatorKey,
+initialRoute: '/login',
+  routes: {
+    "/login":(BuildContext context)=> const LoginPage()
+  },
    );
   }
   
--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,52 +1,79 @@
-
 import 'package:flutter/material.dart';
 
 //Packages
-import "package:firebase_core/firebase_core.dart";
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
 
-//Pages
+//Services
 import '../services/navigation_service.dart';
 import '../services/media_service.dart';
 import '../services/cloud_storage_service.dart';
 import '../services/database_service.dart';
 
-class SplashScreen extends StatefulWidget{
-  const SplashScreen({ required Key key, required this.onIntializationComplete});
-  final VoidCallback onIntializationComplete;
+class SplashScreen extends StatefulWidget {
+  final VoidCallback onInitializationComplete;
+
+  const SplashScreen({
+    required Key key,
+    required this.onInitializationComplete,
+  }) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
-   
     return _SplashScreenState();
   }
-
 }
-  class _SplashScreenState extends State<SplashScreen>{
- @override
-    void initState(){
-      super.initState();
-      _setup().then((_) =>widget.onIntializationComplete );
 
-    }
+class _SplashScreenState extends State<SplashScreen> {
   @override
-  Widget build(BuildContext context) {
-
-   return MaterialApp(title: "Dostify",theme: ThemeData(scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1),
-   ),
-   home: Scaffold(body: Center(child: Container(height: 200,width: 200,decoration:  BoxDecoration(
-      borderRadius: BorderRadius.circular(40),
-    image: DecorationImage(image: AssetImage("assets/images/splash_image.png"),fit: BoxFit.contain),),),),),
-   
-);
-  
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5)).then(
+      (_) {
+        _setup().then(
+          (_) => widget.onInitializationComplete(),
+        );
+      },
+    );
   }
 
-  } 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatify',
+      theme: ThemeData(
+        backgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+        scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              image: DecorationImage(
+                fit: BoxFit.contain,
+                image: AssetImage('assets/images/splash_image.png'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   Future<void> _setup()async{
     WidgetsFlutterBinding.ensureInitialized();
-    await  Firebase.initializeApp();
+    await  Firebase.initializeApp(
+       options: const FirebaseOptions(
+    apiKey: 'AIzaSyBipXQHfaO1FAo0orC7KO5MMgdMzKF_rvI',
+    appId: "1:868733349493:android:7a230dd2e110329764d23c",
+    messagingSenderId: 'sendid',
+    projectId: "dostify-d9fa0",
+    storageBucket: "dostify-d9fa0.appspot.com",
+  )
+    );
     _regiterServices();
   }
 
@@ -57,3 +84,4 @@ GetIt.instance.registerSingleton<CloudStorageService>(CloudStorageService());
 GetIt.instance.registerSingleton<DatabaseService>(DatabaseService());
   }
   
+}

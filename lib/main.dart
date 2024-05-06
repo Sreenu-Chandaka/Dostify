04272fad2d18642be531fc5f22024f408207ff16
Sreenu-Chandaka <chandakasreenu0@gmail.com>
2024-05-06 09:17:20 +0530

    Create| chatuser_model

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:dostify/pages/login_page.dart';
 import 'package:dostify/pages/splash_screen.dart';
+import 'package:dostify/providers/authentication_provider.dart';
 import 'package:flutter/material.dart';
 import "package:firebase_analytics/firebase_analytics.dart";
+import 'package:provider/provider.dart';
 import 'services/navigation_service.dart';
 
 void main() {
@@ -22,15 +24,22 @@ class MainApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Dostify',theme:ThemeData(backgroundColor: const Color.fromRGBO(36, 35, 49, 1),scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1),bottomNavigationBarTheme: const BottomNavigationBarThemeData(backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),)
-   ),
-   navigatorKey:NavigationService.navigatorKey,
-initialRoute: '/login',
-  routes: {
-    "/login":(BuildContext context)=> const LoginPage()
-  },
+   return MultiProvider(
+    providers: [
+      ChangeNotifierProvider<AuthenticationProvider>(create:(context){
+        return AuthenticationProvider();
+      })
+    ],
+     child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dostify',theme:ThemeData(backgroundColor: const Color.fromRGBO(36, 35, 49, 1),scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1),bottomNavigationBarTheme: const BottomNavigationBarThemeData(backgroundColor: Color.fromRGBO(30, 29, 37, 1.0),)
+     ),
+     navigatorKey:NavigationService.navigatorKey,
+     initialRoute: '/login',
+       routes: {
+      "/login":(BuildContext context)=> const LoginPage()
+       },
+     ),
    );
   }
   
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -24,15 +24,21 @@ class LoginPage extends StatefulWidget{
   @override
   Widget build(BuildContext context) {
   _deviceHeight=MediaQuery.of(context).size.height;
- _deviceWidth=MediaQuery.of(context).size.width;
+  _deviceWidth=MediaQuery.of(context).size.width;
    return _buildUI();
   }
 
   Widget _buildUI(){
    
     return Scaffold(body:
-     Container(padding: EdgeInsets.symmetric(horizontal:_deviceWidth * 0.03 ,vertical:_deviceHeight*0.02 ),height: _deviceHeight*0.98,width: _deviceWidth*0.97,
-     child: Column(mainAxisSize: MainAxisSize.max,
+     Container(
+     padding: EdgeInsets.symmetric(horizontal:_deviceWidth * 0.03 ,
+     vertical:_deviceHeight*0.02 ),
+     height: _deviceHeight*0.98,
+     width: _deviceWidth*0.97,
+     child: 
+     Column(
+     mainAxisSize: MainAxisSize.max,
      mainAxisAlignment: MainAxisAlignment.center,
      crossAxisAlignment: CrossAxisAlignment.center,
      children:
@@ -42,12 +48,16 @@ class LoginPage extends StatefulWidget{
      _loginForm(),
       SizedBox(height: _deviceHeight*0.05),
      _loginButton(),
+      SizedBox(height: _deviceHeight*0.02),
      _registerAccountLink()
      ],),),);
   }
  Widget _pageTitle(){
-  return SizedBox(height: _deviceHeight*0.1,child: const Text("Dostify",style: 
-  TextStyle(color: Colors.white,fontSize: 40,fontWeight: FontWeight.w600),),);
+  return SizedBox(height: _deviceHeight*0.1,
+  child: const Text("Dosti",
+  style:TextStyle(color: Colors.white,
+  fontSize: 40,
+  fontWeight: FontWeight.w600),),);
 }
 
 Widget _loginForm(){
@@ -56,16 +66,23 @@ Widget _loginForm(){
     mainAxisAlignment: MainAxisAlignment.spaceBetween,
     crossAxisAlignment: CrossAxisAlignment.center,
     children: [
-      CustomTextFormField(onSaved: (_value){},  regEx:
-                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+", hintText: "Email", obscureText: false),
+      CustomTextFormField(onSaved: (_value){},  
+      regEx: r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+", 
+      hintText: "Email",
+      obscureText: false),
                  
-                    CustomTextFormField(onSaved: (_value){},  regEx:
-                    r".{8,}", hintText: "Password", obscureText: true)],
+      CustomTextFormField(onSaved: (_value){},  
+      regEx:r".{8,}",
+      hintText: "Password",
+      obscureText: true)],
   ),),);
 }
 
 Widget _loginButton(){
-  return RoundedButton(name: "Login", height: _deviceHeight*0.065, width:_deviceWidth* 0.65, onPressed: (){
+  return RoundedButton(name: "Login",
+   height: _deviceHeight*0.065, 
+   width:_deviceWidth* 0.65,
+   onPressed: (){
 
   });
 }
@@ -75,8 +92,9 @@ Widget _registerAccountLink(){
   
    GestureDetector(
     onTap: (){
-      print("Clicked!");
+      debugPrint("Clicked!");
     },
-    child: Container(child: const Text("Don't you have an account?",style: TextStyle(color: Colors.blueAccent),),));
+    child: const Text("Don't you have an account?",
+    style: TextStyle(color: Colors.blueAccent),));
 }
   }

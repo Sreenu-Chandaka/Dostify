dcda27605e42ae23565fa51eb61c09776ea48a5e
Sreenu-Chandaka <chandakasreenu0@gmail.com>
2024-06-05 18:48:14 +0530

    Create|rounded image file

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,6 +1,7 @@
-
-
 //Packages
+// ignore_for_file: unused_element
+
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
@@ -9,33 +10,70 @@ import 'package:file_picker/file_picker.dart';
 //Services
 import '../services/navigation_service.dart';
 
-
 //Widgets
 import '../widgets/custom_input_field.dart';
 import '../widgets/rounded_button.dart';
+import '../widgets/rounded_image.dart';
 
 //Providers
 import '../providers/authentication_provider.dart';
 
-
-
-
-class RegisterPage extends StatefulWidget{
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
   @override
   State<StatefulWidget> createState() {
-   return _RegisterPageState();
+    return _RegisterPageState();
   }
-  
 }
 
-class _RegisterPageState extends State<RegisterPage>{
+class _RegisterPageState extends State<RegisterPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+  PlatformFile? _profileImage;
   @override
   Widget build(BuildContext context) {
-  return _buildUI();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return _buildUI();
   }
-  Widget _buildUI(){
-    return const Scaffold();
+
+  Widget _buildUI() {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth * 0.03, vertical: _deviceHeight * 0.02),
+        width: _deviceWidth * 0.98,
+        height: _deviceHeight * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _profileImageField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _profileImageField() {
+    return GestureDetector(
+        onTap: () {
+          FilePicker.platform;
+        },
+        child: () {
+          if (_profileImage != null) {
+            return RoundedImageFile(
+                key: UniqueKey(),
+                size: _deviceHeight * 0.15,
+                image: _profileImage!);
+          }
+          return RoundedImageNetwork(
+            imagePath: 'https://i.pravatar.cc/1000?img=3',
+            size: _deviceHeight * 0.15,
+            key: UniqueKey(),
+          );
+        }());
   }
 }
---
+++ lib/widgets/rounded_image.dart
@@ -0,0 +1,44 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+class RoundedImageNetwork extends StatelessWidget {
+  final double size;
+  final String imagePath;
+  const RoundedImageNetwork(
+      {required Key key, required this.size, required this.imagePath})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size,
+      width: size,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: NetworkImage(imagePath), fit: BoxFit.cover),
+          borderRadius: BorderRadius.all(Radius.circular(size)),
+          color: Colors.black),
+    );
+  }
+}
+
+class RoundedImageFile extends StatelessWidget {
+  final double size;
+  final PlatformFile image;
+  const RoundedImageFile(
+      {required Key key, required this.size, required this.image})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size,
+      width: size,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(image.path!), fit: BoxFit.cover),
+          borderRadius: BorderRadius.all(Radius.circular(size)),
+          color: Colors.black),
+    );
+  }
+}

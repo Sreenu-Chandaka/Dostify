728a5cb6ced130b6bd6fe309bce0d1894e8fffc2
Sreenu-Chandaka <chandakasreenu0@gmail.com>
2024-06-10 12:07:13 +0530

    Create| chat model

---
+++ lib/models/chat.dart
@@ -0,0 +1,35 @@
+//Models
+// ignore_for_file: no_leading_underscores_for_local_identifiers, unused_field
+
+import '../models/chat_message.dart';
+import '../models/chat_user.dart';
+
+class Chat {
+  final String uid, currentUserUid;
+  final bool activity, group;
+  final List<ChatUser> members;
+  List<ChatMessage> messages;
+  late final List<ChatUser> _recepients;
+
+  Chat(
+      {required this.uid,
+      required this.currentUserUid,
+      required this.activity,
+      required this.group,
+      required this.members,
+      required this.messages}){
+        _recepients=members.where((_i) => _i.uid!=currentUserUid).toList();
+      }
+
+      List<ChatUser> recepients(){
+        return _recepients;
+      }
+      String title(){
+        return !group?_recepients.first.name:_recepients.map((_user) => _user.name).join(', ');
+
+      }
+
+      String imageURL(){
+        return !group?_recepients.first.imageURL:"https://cdn-icons-png.flaticon.com/512/10728/10728169.png";
+      }
+}
--- lib/pages/chats_page.dart
+++ lib/pages/chats_page.dart
@@ -29,19 +29,29 @@ class _ChatsPageState extends State<ChatsPage> {
 
   Widget _buildUI() {
     return Container(
-        padding: EdgeInsets.symmetric(
-            horizontal: _deviceWidth * 0.03, vertical: _deviceHeight * 0.02),
-        height: _deviceHeight * 0.98,
-        width: _deviceWidth * 0.97,
-        child:  Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TopBar("Chats",primaryAction: IconButton(icon: Icon(Icons.logout,color: Color.fromRGBO(0,82,218,1.0),),onPressed: (){
-
-            },),)
-          ],
-        ),);
+      padding: EdgeInsets.symmetric(
+          horizontal: _deviceWidth * 0.03, vertical: _deviceHeight * 0.02),
+      height: _deviceHeight * 0.98,
+      width: _deviceWidth * 0.97,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TopBar(
+            "Chats",
+            primaryAction: IconButton(
+              icon: const Icon(
+                Icons.logout,
+                color: Color.fromRGBO(0, 82, 218, 1.0),
+              ),
+              onPressed: () {
+                _auth.logOut();
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }

58915743531136061a221d83e5d85a291da5f6c8
Sreenu-Chandaka <chandakasreenu0@gmail.com>
2024-06-10 17:19:28 +0530

    Update| chats provider business logic added to UI

--- lib/pages/chats_page.dart
+++ lib/pages/chats_page.dart
@@ -5,8 +5,10 @@ import "package:provider/provider.dart";
 
 //Providers
 import "package:dostify/providers/authentication_provider.dart";
+import "../providers/chats_page_provider.dart";
 
 //Widgets
+
 import '../widgets/custom_listview_tiles.dart';
 import "package:dostify/widgets/top_bar.dart";
 
@@ -23,48 +25,64 @@ class _ChatsPageState extends State<ChatsPage> {
   late double _deviceHeight;
   late double _deviceWidth;
   late AuthenticationProvider _auth;
+  late ChatsPageProvider _pageProvider;
 
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
     _auth = Provider.of<AuthenticationProvider>(context);
-    return _buildUI();
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (_) => ChatsPageProvider(_auth)),
+    ], child: _buildUI());
   }
 
   Widget _buildUI() {
-    return Container(
-      padding: EdgeInsets.symmetric(
-          horizontal: _deviceWidth * 0.03, vertical: _deviceHeight * 0.02),
-      height: _deviceHeight * 0.98,
-      width: _deviceWidth * 0.97,
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          TopBar(
-            "Chats",
-            primaryAction: IconButton(
-              icon: const Icon(
-                Icons.logout,
-                color: Color.fromRGBO(0, 82, 218, 1.0),
+    return Builder(
+      builder: (BuildContext _context) {
+        _pageProvider=_context.watch<ChatsPageProvider>();
+        return Container(
+          padding: EdgeInsets.symmetric(
+              horizontal: _deviceWidth * 0.03, vertical: _deviceHeight * 0.02),
+          height: _deviceHeight * 0.98,
+          width: _deviceWidth * 0.97,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TopBar(
+                "Chats",
+                primaryAction: IconButton(
+                  icon: const Icon(
+                    Icons.logout,
+                    color: Color.fromRGBO(0, 82, 218, 1.0),
+                  ),
+                  onPressed: () {
+                    _auth.logOut();
+                  },
+                ),
               ),
-              onPressed: () {
-                _auth.logOut();
-              },
-            ),
+              _chatList()
+            ],
           ),
-          CustomListViewTilesWithActivity(
-              height: _deviceHeight * 0.1,
-              title: "Sreenu Chandaka",
-              subtitle: "hello",
-              imagePath: 'https://i.pravatar.cc/300',
-              isActive: false,
-              isActivity: false,
-              onTap: () {})
-        ],
-      ),
+        );
+      }
     );
+  }
+
+  Widget _chatList() {
+    return _chatTile();
+  }
+
+  Widget _chatTile() {
+    return CustomListViewTilesWithActivity(
+        height: _deviceHeight * 0.1,
+        title: "Sreenu Chandaka",
+        subtitle: "hello",
+        imagePath: 'https://i.pravatar.cc/300',
+        isActive: false,
+        isActivity: false,
+        onTap: () {});
   }
 }
---
+++ lib/providers/chats_page_provider.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+//Packages
+import "package:flutter/material.dart";
+import 'package:get_it/get_it.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+//Services
+import '../services/database_service.dart';
+
+//Providers
+import '../providers/authentication_provider.dart';
+
+//Models
+import "../models/chat.dart";
+import "../models/chat_message.dart";
+
+class ChatsPageProvider extends ChangeNotifier {
+  AuthenticationProvider _auth;
+  late DatabaseService _db;
+  List<Chat>? chats;
+  late StreamSubscription _chatsStream;
+
+  ChatsPageProvider(this._auth) {
+    _db = GetIt.instance.get<DatabaseService>();
+    getChats();
+  }
+
+  @override
+  void dispose() {
+    _chatsStream.cancel();
+    super.dispose();
+  }
+
+  void getChats() async {}
+}

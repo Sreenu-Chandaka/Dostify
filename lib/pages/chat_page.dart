4b48a9077a07cbec2a007498fec2fadea5ca6db9
Sreenu-Chandaka <chandakasreenu0@gmail.com>
2024-06-19 11:12:37 +0530

    Add| topbar to chatpage

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:dostify/models/chat.dart';
 import 'package:dostify/providers/authentication_provider.dart';
+import 'package:dostify/widgets/top_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,7 +15,6 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatsPageState extends State<ChatPage> {
-
   late double _deviceHeight;
   late double _deviceWidth;
   late AuthenticationProvider _auth;
@@ -23,13 +23,51 @@ class _ChatsPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    _deviceHeight=MediaQuery.of(context).size.height;
-    _deviceWidth=MediaQuery.of(context).size.width;
-    _auth=Provider.of<AuthenticationProvider>(context);
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthenticationProvider>(context);
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth * 0.03,
+            vertical: _deviceHeight * 0.02,
+          ),
+          height: _deviceHeight * 0.98,
+          width: _deviceWidth * 0.97,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TopBar(
+                widget.chat.title(),
+                fontSize: 14,
+                primaryAction: IconButton(
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Color.fromRGBO(0, 82, 218, 1.0),
+                  ),
+                  onPressed: () {},
+                ),
+                secondaryAction: IconButton(
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Color.fromRGBO(0, 82, 218, 1.0),
+                  ),
+                  onPressed: () {
+                    
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

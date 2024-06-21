bbf2180ab8d28b5ff0de194714019a0a049c718b
Sreenu <srinu.c@orotron.com>
2024-06-21 16:12:31 +0530

    Update| GetHelper class

--- lib/helper/get_helper.dart
+++ lib/helper/get_helper.dart
@@ -1,33 +1,14 @@
 import 'package:get_storage/get_storage.dart';
 
 class GetHelper {
-  static GetHelper? _instance;
-  static GetStorage _box=GetStorage();
+  static const String accessToken = 'access-token';
 
-  GetHelper._internal();
+  static final box = GetStorage();
 
-  /// Singleton instance of GetHelper.
-  static Future<GetHelper> getInstance() async {
-    if (_instance == null) {
-      _instance = GetHelper._internal();
-      await _initializeStorage();
-    }
-    return _instance!;
-  }
-
-  /// Initialize GetStorage.
-  static Future<void> _initializeStorage() async {
+  static Future<void> init() async {
     await GetStorage.init();
-    _box = GetStorage();
   }
 
-  /// Sets the current user ID.
- static void setCurrentUserId(String userId) {
-    _box.write('user_id', userId);
-  }
-
-  /// Gets the current user ID.
- static String getCurrentUserId() {
-    return _box.read('user_id');
-  }
+  static String getAccessToken() => box.read(accessToken) ?? '';
+  static void setAccessToken(String value) => box.write(accessToken, value);
 }
